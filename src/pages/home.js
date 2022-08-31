bf30c27c0b30f28826ea25bea972dc1a3722bf35
anbusivan <anbusivan@gmail.com>
2022-08-31 11:04:12 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,7 +17,7 @@ const Home = () => {
       <div className="LandinPage">
        <img src={MyImage}></img>
        <h3 className='heading3'>"The soul of India lives in its villages" - Mahatma Gandhi. </h3>
-       <p className='heading3'>My village is a petit abode for happy people.  Here they live in harmony and peace. </p>
+       <p className='heading3'>our village is a verdant and petit abode for happy people.  Here they live in harmony and peace. </p>
        <p className='heading3'>The village people are very hardworking and thus they must have a good source of upliftment.</p>
        <p className='heading3'>The farmers should be facilitated by helping them to leverage their sales, make them notified about the modern techniques used in farming, and give them compensation and knowledge of the true market price of their yield.</p>
       </div>
